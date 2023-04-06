7be657ba0dd28e6f02e523a00c344687c8fda048
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-06 15:42:59 +0200

    Snacks1

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+ namespace Snacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Snacks1();
+        }
+
+        //1. void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]
+        public static void Snacks1()
+        {
+            Console.WriteLine("Snack1: ");
+            int[] arrNum = { 12, 23, 34, 45, 56, 67, 78, 89, 90, 101};
+            StampaArray(arrNum);
+
+            void StampaArray(int[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"Elemento {i + 0}: {array[i]}");
+                }
+            }
+        }
+    }
+}
