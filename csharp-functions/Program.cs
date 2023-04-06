dd1f29a441c302eae43d9450eb01d42dc5b90bc2
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-06 16:01:30 +0200

    Snacks2 + Add comms

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,9 +4,10 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Snacks1();
+            Snacks2();
         }
 
         //1. void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]
@@ -16,13 +17,32 @@
             int[] arrNum = { 12, 23, 34, 45, 56, 67, 78, 89, 90, 101};
             StampaArray(arrNum);
 
-            void StampaArray(int[] array)
+            static void StampaArray(int[] array)
             {
                 for (int i = 0; i < array.Length; i++)
                 {
                     Console.WriteLine($"Elemento {i + 0}: {array[i]}");
                 }
             }
+            Console.WriteLine("");
+        }
+
+
+        //2. int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+        public static void Snacks2()
+        {
+            Console.WriteLine("Snacks2:");
+            Console.WriteLine("Inserisci un numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Quadrato(num);
+
+            static int Quadrato(int n)
+            {
+                int res = n * n;
+                Console.WriteLine($"Risultato:{res}");
+                return res;
+            }
+            Console.WriteLine("");
         }
     }
 }
